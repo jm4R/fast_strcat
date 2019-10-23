5d40f2eae78074d0e471302e1766284cfd5352f6
jm4R <mar.jaskolka@gmail.com>
2019-10-23 19:31:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# FastStrcat
+
+The header-only library that helps with single allocation std::basic_string building.
+
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <fast_strcat/strcat.hpp>
+#include <fast_strcat/string_builder.hpp>
+
+
+int main()
+{
+	return 0;
+}
